6f386379a0f6bd6967d0b3caf6fb3d4f56cbd377
pzy66 <158559265+pzy66@users.noreply.github.com>
2024-07-19 09:52:53 +0800

    改变计数器

--- Project_TIMER/Project_TIMER/Segment.h
+++ Project_TIMER/Project_TIMER/Segment.h
@@ -1,8 +1,8 @@
 //ÊıÂë¹Ü
-sbit SegmentG1 = P2 ^ 3;   
-sbit SegmentG2 = P2 ^ 2;
-sbit SegmentG3 = P2 ^ 1;
-sbit SegmentG4 = P2 ^ 0;
+sbit SegmentG4 = P2 ^ 3;   
+sbit SegmentG3 = P2 ^ 2;
+sbit SegmentG2 = P2 ^ 1;
+sbit SegmentG1 = P2 ^ 0;
 
 extern unsigned char leddata[];
 
--- Project_TIMER/Project_TIMER/main.c
+++ Project_TIMER/Project_TIMER/main.c
@@ -6,7 +6,7 @@
 #include "EEPROM.h"
 #include "output.h"
 
-unsigned long sec_count = 0; // 秒计数
+unsigned int sec_count = 0; // 秒计数
 unsigned int day_count = 1;  // 天计数，初始值为1
 unsigned int year = 2024;    // 年份，初始值为2024
 unsigned int days_in_year =0;
@@ -22,21 +22,24 @@ char buffer[26]; // 调整大小以适应格式化字符串
 // 定时器0中断服务程序
 void Timer0_Handler() interrupt 1 {
   static unsigned int count = 0; // 定时器溢出计数
-  count++;
-  if (count >= 574) {
-    count = 0;
-    if (sec_count < 86400) {
-      sec_count++;
+    count++;
+    if (count >= 20) { // 每50ms溢出一次，20次溢出大约1秒
+        count = 0;
+        if (sec_count < 86400) { // 一天有86400秒
+            sec_count++;
+        } else {
+            sec_count = 0;
+            day_count++;
+            days_in_year = 365 + ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0) ? 1 : 0);
+            if (day_count > days_in_year) {
+                day_count = 1;
+                year++;
+            }
+        }
     }
-    else {
-      sec_count = 0;
-      day_count++;
-      days_in_year = 365 + ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0) ? 1 : 0);
-      if (day_count > days_in_year) {
-        day_count = 1;
-        year++;
-      }
-    }
+    // 重新加载定时器初始值
+    TH0 = 0x8A;
+    TL0 = 0xD0;
     
         month = ana_month(day_count,year);
         day = ana_day(day_count,year);
@@ -44,36 +47,13 @@ void Timer0_Handler() interrupt 1 {
         min = ana_min(sec_count);
         hour = ana_hour(sec_count);
 
-        buffer[0] = get(year, 4)+'0';
-        buffer[1] = get (year, 3)+'0';
-        buffer[2] = get (year, 2)+'0';
-        buffer[3] = get (year, 1)+'0';
-        buffer[4] = '-';
-        buffer[5] = get (month, 2)+'0';
-        buffer[6] = get (month, 1)+'0';
-        buffer[7] = '-';
-        buffer[8] = get (day, 2)+'0';
-        buffer[9] = get (day, 1)+'0';
-        buffer[10] = ' ';
-        buffer[11] = get (hour, 2)+'0';
-        buffer[12] = get (hour, 1)+'0';
-        buffer[13] = ':';
-        buffer[14] = get (min, 2)+'0';
-        buffer[15] = get (min, 1)+'0';
-        buffer[16] = ':';
-        buffer[17] = get (sec, 2)+'0';
-        buffer[18] = get (sec, 1)+'0';
-        buffer[19] = ' ';
-        buffer[20] = get_weekday(day_count)[0];
-        buffer[21] = get_weekday(day_count)[1];
-        buffer[22] = get_weekday(day_count)[2];
-        buffer[23] = '\r';
-        buffer[24] = '\n';
-        buffer[25] = '\0'; // 以空字符结尾，标志字符串的结束
-
+        
+        display_time(sec_count);
         UART_SendString(buffer); // 发送到UART
+       
+       TH0 = 0x8A;    //重载定时器初值
+        TL0 = 0xD0;
   }
-}
 
 void uart_isr() interrupt 4 {
     if (RI) {
@@ -92,7 +72,7 @@ void main() {
     Timer0_Init(); //定时器1 100微妙
     InitUART();// 串口初始化函数
     while (1) {
-        display_time(sec_count);
+      
     }
 }
 
